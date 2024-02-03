4e424c2ba87aa9c7c51b840ecff24b29b0cb67cb
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-02-03 15:41:28 +0530

    Fixed css and added about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+import Script from "next/script"
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+
+export default function about(){
+  return (
+    <div>about</div>
+  )
+}
