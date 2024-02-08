4e5fccd6f875e495421050f229e1ce0e3e59647c
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-02-09 00:13:31 +0530

    Made blog page dynamic

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 import styles from '../../styles/Blogpost.module.css'
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
+    const [blog,setBlog] = useState();
+
+    useEffect(() =>
+    {
+       if(!router.isReady)return;
+
+       fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a)=>{
+          return a.json()
+       }).then((data)=>{
+          setBlog(data);
+       })
+    },[router.isReady])
 
   return (
     <div className={styles.container}>
        <main className={styles.main}>
-       <h1>Title of the page : {slug}</h1>
+       <h1>Title of the page : {blog && blog.title}</h1>
        <hr/>
        <div>
-          {slug} is an amazing technology. Read the page to get to know entire working and advantages and drawbacks of {slug}
+          {blog && blog.content}
        </div>
        </main>
     </div>
