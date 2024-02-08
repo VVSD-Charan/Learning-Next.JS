f40007f720515e068c7c37e9165d7c57abef8a6f
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-02-08 11:48:18 +0530

    Added styles to blogpost and blog pages

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import { Inter } from "next/font/google";
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.container}>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className="blogs">
+          <h2>Popular blogs</h2>
+          <div className={styles.blogItemh3}>
+            <Link href={'/blogpost/learn-javascript'}>
+              <h3>How to learn c++ in 2024 ?</h3>
+            </Link>
+            <p>C++ is a object oriented language</p>
+          </div>
+          <div className={styles.blogItemh3}>
+            <h3>How to learn c++ in 2024 ?</h3>
+            <p>C++ is a object oriented language</p>
+          </div>
+          <div className={styles.blogItemh3}>
+            <h3>How to learn c++ in 2024 ?</h3>
+            <p>C++ is a object oriented language</p>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useRouter } from 'next/router';
+import styles from '../../styles/Blogpost.module.css'
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
 
   return (
-    <div>{slug}</div>
+    <div className={styles.container}>
+       <main className={styles.main}>
+       <h1>Title of the page : {slug}</h1>
+       <hr/>
+       <div>
+          {slug} is an amazing technology. Read the page to get to know entire working and advantages and drawbacks of {slug}
+       </div>
+       </main>
+    </div>
   )
 }
 
